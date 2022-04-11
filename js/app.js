a27cd8eb8e9b3f70f562e25e734007e39ef6975e
cumbinar <cumbiweb@gmail.com>
2022-04-10 20:26:41 -0500

    comparador estricto

--- js/app.js
+++ js/app.js
@@ -15,3 +15,23 @@ if (puntaje == 1000) {
 } else {
     console.log('puntaje no es igual a 1000');
 }
+
+const contador = 800;
+
+console.log(contador);
+if (contador != 500) {
+    console.log('si es diferente de 500');
+} else {
+    console.log('no esdiferente');
+}
+
+
+const numero = 1000;
+
+if (numero === "1000") {
+    console.log("si es igual de forma estricta")
+} else {
+    console.log('No es igual estrictamente');
+}
+
+/*  !== (diferente estricto), ===(igual estricto)  */
